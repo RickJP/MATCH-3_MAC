aaee1acb3919e83bfdfc9d2b3a3df82caf2565b2
Dev42013 <rickd1977@gmail.com>
2018-03-07 18:39:39 +0900

    Added board of tiles

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class Board : MonoBehaviour {
 
+    public int width, height;
+    public GameObject tilePrefab;
+    public int borderSize;
+
+    Tile[,] m_allTiles;
+
 	// Use this for initialization
 	void Start () {
-		
+        m_allTiles = new Tile[width, height];
+        SetupTiles();
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+    void SetupTiles()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                GameObject tile = Instantiate(tilePrefab, new Vector3(i, j, 0), Quaternion.identity) as GameObject;
+
+                tile.name = "Tile (" + i + "," + j + ")";
+
+                m_allTiles[i, j] = tile.GetComponent<Tile>();
+
+                tile.transform.parent = transform;
+
+            }
+        }
+    }
+
+    void SetupCamera()
+    {
+        Camera.main.transform.position = new Vector3((float)(width - 1) / 2f, (float)(height - 1)/ 2f, -10f);
+
+        float aspectRatio = (float)Screen.width / (float)Screen.height;
+
+        float verticalSize = (float)height / 2f + (float)borderSize;
+
+        float horizontalSize = ((float)width / 2f + (float)borderSize) / aspectRatio;
+
+        Camera.main.orthographicSize = (verticalSize > horizontalSize) ? verticalSize : horizontalSize;
+    }
 }
